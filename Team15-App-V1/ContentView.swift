7927e5908cb491be45ccb92de83476e391496f1e
Edward Chen <edward.chen@mini-ivy.com>
2022-11-05 14:04:37 -0400

    Initial Commit

---
+++ Team15-App-V1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Team15-App-V1
+//
+//  Created by Edward Chen on 11/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Team15-App-V1/Team15_App_V1App.swift
@@ -0,0 +1,17 @@
+//
+//  Team15_App_V1App.swift
+//  Team15-App-V1
+//
+//  Created by Edward Chen on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Team15_App_V1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
