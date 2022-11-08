fdc94a5e644b3510bea4bdce2a5209f928d10be1
Philip Wellener <philipwellener@gmail.com>
2022-11-08 10:28:24 -0500

    Home view outline

---
+++ Team15-App-V1/Views/Home/HomeGalleryView.swift
@@ -0,0 +1,37 @@
+//
+//  HomeGalleryView.swift
+//  Team15-App-V1
+//
+//  Created by Philip Wellener on 11/7/22.
+//
+
+import SwiftUI
+
+struct HomeGalleryView: View {
+  var body: some View {
+    ScrollView {
+//      LazyVGrid(
+//        columns: Array(
+//          repeating: .init(.adaptive(minimum: 100), spacing: 1),
+//          count: 2
+//        ),
+//        spacing: 1
+//      ) {
+//        ForEach(Updates.results, id: \.self) { asset in
+//
+//          Button {
+//            // TODO: Add tapping action here
+//          } label: {
+//            UpdatesDeatilView(assetLocalId: asset.localIdentifier)
+//          }
+//        }
+//      }
+    }
+  }
+}
+
+struct HomeGalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeGalleryView()
+    }
+}
--- Team15-App-V1/Views/Home/HomeView.swift
+++ Team15-App-V1/Views/Home/HomeView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Home")
+          Text("Welcome").font(.largeTitle)
+          
+          Spacer()
         }
         .padding()
     }
